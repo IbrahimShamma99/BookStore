f87b30838101a7b2c004961d27b8f01b365f4f68
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-22 03:59:07 +0300

    👌 IMPROVE:

--- ServerSide/Book/api.js
+++ ServerSide/Book/api.js
@@ -8,13 +8,13 @@ const bookParams = require("./params");
 router.param(params.book, bookParams);
 
 //SECTION add book
-router.post(Routes.create,helper.required, controlers.adduser);
+router.post(Routes.create,helper.required, uploadCover.single("cover"), controlers.adduser);
 
 //SECTION fetch book
 router.post(Routes.fetch,helper.optional , controlers.adduser);
 
 //SECTION update book
-router.post(Routes.update,helper.required, controlers.adduser);
+router.post(Routes.update,helper.required,uploadCover.single("cover"), ,controlers.adduser);
 
 
 module.exports = router;
