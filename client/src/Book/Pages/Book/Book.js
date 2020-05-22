0b2c9b6b4ffda4a9fae7a47b92b01b9fb2f9fd24
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-22 05:01:35 +0300

    👌 IMPROVE:

--- client/src/Book/Pages/Book/Book.js
+++ client/src/Book/Pages/Book/Book.js
@@ -1,10 +1,27 @@
 import React from "react";
 
+const BookInfo = {
+  _id: "0000",
+  title: "EGO",
+  brief: "GOOD BOOK",
+  author: "RYAN",
+  owner: "jenn",
+  reviews: [
+    { writer: "jaaws", body: "Excellent" },
+    { writer: "easyMoneySniper", body: "Great" },
+  ],
+  cover: {
+    filename: "XX",
+  },
+};
+
 class Book extends React.Component {
   render() {
-    return (<div>
-            Book
-        </div>);
+    return <div>{
+      Object.keys(BookInfo).map(key=>{
+        return <p>{BookInfo[key]}</p>
+      })
+    }</div>;
   }
 }
 export default Book;
