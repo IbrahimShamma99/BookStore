e57e4b3bbeea9935b2ad0c298ce2cc7351d1325e
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-22 04:29:08 +0300

    ‼️ BREAKING:

--- ServerSide/server/server.js
+++ ServerSide/server/server.js
@@ -1,52 +1,52 @@
 const app = require("./intialize");
 const mongoose = require("mongoose");
-const LocalDB = require('../config/config').LocalDB;
-var isProduction = process.env.NODE_ENV === 'production';
+const LocalDB = require("../config/config").LocalDB;
+var isProduction = process.env.NODE_ENV === "production";
 
 //SECTION Import schemas
-const User = require('../User/schema');
+const User = require("../User/schema");
+const Book = require("../Book/schema");
 
 //SECTION ADMINBRO
-const AdminBro = require('admin-bro')
-const AdminBroExpressjs = require('admin-bro-expressjs')
-const AdminBroMongoose = require('admin-bro-mongoose')
-AdminBro.registerAdapter(AdminBroMongoose)
+const AdminBro = require("admin-bro");
+const AdminBroExpressjs = require("admin-bro-expressjs");
+const AdminBroMongoose = require("admin-bro-mongoose");
+AdminBro.registerAdapter(AdminBroMongoose);
 
 //SECTION Importing components
-var userComponent = require("../User/index");
+var userComponent = require("../User/");
+var bookComponent = require("../Book/");
 
 //SECTION Using API Components
 app.use(userComponent);
-
+app.use(bookComponent);
 var DB = null;
 //SECTION Connecting to MongoDB
 if (isProduction) {
-    mongoose.connect(process.env.MONGODB_URI, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-    });
+  mongoose.connect(process.env.MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  });
 } else {
-    DB = mongoose.connect(LocalDB, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-    });
-    mongoose.set('debug', true);
+  DB = mongoose.connect(LocalDB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  });
+  mongoose.set("debug", true);
 }
 
 const adminBro = new AdminBro({
-    resources: [
-        {resource: User}
-        ],
-      database:DB,
-      branding: {
-        companyName: 'Learn-Web-Community',
-      },
-          rootPath: '/admin',
-  })
-const router = AdminBroExpressjs.buildRouter(adminBro)
-app.use(adminBro.options.rootPath, router)
+  resources: [{ resource: User }, { resource: Book }],
+  database: DB,
+  branding: {
+    companyName: "Learn-Web-Community",
+  },
+  rootPath: "/admin",
+});
+const router = AdminBroExpressjs.buildRouter(adminBro);
+app.use(adminBro.options.rootPath, router);
 
 //NOTE exporting app to avoid singleton violation
 module.exports = app;
