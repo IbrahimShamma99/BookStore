597784f945bed4cfe62b6a76aaa63e995e267a30
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-23 22:42:36 +0300

    👌 IMPROVE:

--- client/src/Book/Store/book.actions.js
+++ client/src/Book/Store/book.actions.js
@@ -1,3 +1,5 @@
 export const FETCH_BOOK = () => "FETCH_BOOK";
 export const CREATE_BOOK = () => "CREATE_BOOK";
 export const MODIFY = () => "MODIFY";
+export const SUCCESS = () => "SUCCESS";
+export const ERROR = () => "ERROR";
--- client/src/Book/Store/book.reducers.js
+++ client/src/Book/Store/book.reducers.js
@@ -1,12 +1,22 @@
 import * as BookActions from "./book.actions";
-import initialState from './book.constants';
+import initialState from "./book.constants";
+import * as api from "../Utils/api";
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case BookActions.CREATE_BOOK:
+      api.create().then(data=>{
+        if (data.error){
+          action.asyncDispatch({type:BookActions.SUCCESS,message:data.error})
+        }
+        else {
+          action.asyncDispatch({type:BookActions.SUCCESS,data})
+        }
+      })
+      return {...state}
     case BookActions.FETCH_BOOK:
     default:
-        return {...state}
+      return { ...state };
   }
 };
 
