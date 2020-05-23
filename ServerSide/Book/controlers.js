6b0f7719b23c51ed4ec63b75c1ad59f868b9477a
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-23 15:23:48 +0300

    👌 IMPROVE:

--- ServerSide/Book/controlers.js
+++ ServerSide/Book/controlers.js
@@ -5,6 +5,15 @@ var Book = mongoose.model("Book");
 const create = (req, res) => {
   const UserID = req.query.user;
   const bookInfo = req.body.book;
+  if (!UserID) {
+    return res.status(422).send({ error: "user not found" });
+  }
+  if (!bookInfo) {
+    return res.status(422).send({ error: "book info not found" });
+  }
+  if (!bookInfo.title) {
+    return res.status(422).send({ error: "book title not provided" });
+  }
   const book = new Book();
   book.assignInfo(bookInfo);
   book.save(() => {
--- ServerSide/helper/paths.js
+++ ServerSide/helper/paths.js
@@ -4,9 +4,9 @@ const publicDir = path.join(__dirname, "../public/");
 const exportedAvatarPath = path.join(publicDir, "avatar/");
 const exportedCoverPath = path.join(publicDir, "cover/");
 
-console.log(" publicDir", publicDir)
-console.log("exportedAvatarPath",exportedAvatarPath)
-console.log("exportedCoverPath",exportedCoverPath)
+console.log("\u{1F3CB}\u{FE0F}\u{200D}\u{2642}\u{FE0F} publicDir=", publicDir)
+console.log("\u{1F9D8}\u{200D}\u{2640}\u{FE0F} exportedAvatarPath=",exportedAvatarPath)
+console.log("\u{1F955} exportedCoverPath=",exportedCoverPath)
 
 const paths = {
   publicDir: publicDir,
