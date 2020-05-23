f26a36ae28b13aad880a477701f3618d04719212
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-23 06:34:22 +0300

    👌 IMPROVE:

--- ServerSide/helper/paths.js
+++ ServerSide/helper/paths.js
@@ -3,6 +3,11 @@ var path = require("path");
 const publicDir = path.join(__dirname, "../public/");
 const exportedAvatarPath = path.join(publicDir, "avatar/");
 const exportedCoverPath = path.join(publicDir, "cover/");
+
+console.log(" publicDir", publicDir)
+console.log("exportedAvatarPath",exportedAvatarPath)
+console.log("exportedCoverPath",exportedCoverPath)
+
 const paths = {
   publicDir: publicDir,
   userAvatar: exportedAvatarPath,
--- client/src/USER/Pages/Profile/Profile.js
+++ client/src/USER/Pages/Profile/Profile.js
@@ -33,24 +33,23 @@ class Profile extends React.Component {
   render() {
     return (
       <div className="container">
-
         <Breakpoint medium up>
           {/** Desktop & Tablet version */}
           <this.Styles.ProfileContainer
             primary={this.props.theme === "light" ? true : null}
           >
-          {this.props.show ? (
-            <div className="alert">
-              <span
-                className="closebtn"
-                onClick="this.parentElement.style.display='none';"
-              >
-                &times;
-              </span>
-              {this.props.error}
-            </div>
-          ) : null}
-  
+            {this.props.show ? (
+              <div className="alert">
+                <span
+                  className="closebtn"
+                  onClick="this.parentElement.style.display='none';"
+                >
+                  &times;
+                </span>
+                {this.props.error}
+              </div>
+            ) : null}
+
             {this.props.profile.avatar ? (
               this.props.profile.avatar.filename ? (
                 <div class="view overlay zoom">
