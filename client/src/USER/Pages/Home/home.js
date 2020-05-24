d43d7d34da6dd98413cf42530403e7868db3efda
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-25 02:53:42 +0300

    👌 IMPROVE:

--- client/src/USER/Pages/Home/home.js
+++ client/src/USER/Pages/Home/home.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./home.scss";
 import { connect } from "react-redux";
 import * as actionTypes from "../../Store/user.actions";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const mapStatetoProps = (state) => {
   return {
@@ -18,7 +18,27 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 const HomePage = styled.div`
+  & > img {
+    position: absolute;
+    top: 70px;
+    left: 0px;
+    width: 100%;
+    height: 500px;
+  }
+  & > h1 {
+    position: absolute;
+    left: 40%;
+    top:50px;
 
+  }
+  & > span {
+    color: white;
+    font: bold 24px/45px Helvetica, Sans-Serif;
+    letter-spacing: -1px;
+    background: rgb(0, 0, 0); /* fallback color */
+    background: rgba(0, 0, 0, 0.7);
+    padding: 10px;
+  }
 `;
 
 class Home extends React.Component {
@@ -26,6 +46,9 @@ class Home extends React.Component {
     return (
       <HomePage>
         <img src={require("../../../logos/home.jpg")}></img>
+        <h1>
+          <span>BookStore</span>
+        </h1>
       </HomePage>
     );
   }
