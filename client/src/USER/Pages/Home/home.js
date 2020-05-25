d45ff9fe0d053658aac92858d19b0414ab853f4e
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-25 03:19:21 +0300

    👌 IMPROVE:

--- client/src/USER/Pages/Home/home.js
+++ client/src/USER/Pages/Home/home.js
@@ -23,21 +23,46 @@ const HomePage = styled.div`
     top: 70px;
     left: 0px;
     width: 100%;
-    height: 500px;
+    height: 100%;
   }
+`;
+const TextArea = styled.div`
   & > h1 {
     position: absolute;
-    left: 40%;
-    top:50px;
-
+    left: 35%;
+    top: 50px;
   }
-  & > span {
-    color: white;
-    font: bold 24px/45px Helvetica, Sans-Serif;
-    letter-spacing: -1px;
-    background: rgb(0, 0, 0); /* fallback color */
-    background: rgba(0, 0, 0, 0.7);
-    padding: 10px;
+  & button {
+  }
+`;
+
+const Span = styled.span`
+  color: white;
+  font: bold 24px/45px Helvetica, Sans-Serif;
+  letter-spacing: -1px;
+  background: rgb(0, 0, 0); /* fallback color */
+  background: rgba(0, 0, 0, 0.7);
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  position: absolute;
+  left: 38%;
+  top: 57%;
+  background-color: rgb(180, 55, 55);
+  color: black;
+  width: 130px;
+  font-weight:bold;
+  height: 42px;
+  border-radius: 6px;
+  font-weight: bold;
+  outline: invert;
+  margin-left: 75px;
+  margin-right: auto;
+  &: hover {
+    background-color: azure;
+    font-weight:bolder;
+    border-color:rgb(180, 55, 55);
   }
 `;
 
@@ -46,9 +71,13 @@ class Home extends React.Component {
     return (
       <HomePage>
         <img src={require("../../../logos/home.jpg")}></img>
-        <h1>
-          <span>BookStore</span>
-        </h1>
+        <TextArea>
+          <h1>
+            <Span>Welcome to BookStore</Span>
+          </h1>
+          <a href="/register"
+          ><Button>Join Now</Button></a>
+        </TextArea>
       </HomePage>
     );
   }
