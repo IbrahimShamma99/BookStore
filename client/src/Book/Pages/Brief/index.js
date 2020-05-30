eb17d0af4a5f275cb842cae729f97c3cb16d18b7
ibrahimshamma99 <i.abushammah@gmail.com>
2020-05-30 18:32:33 +0300

    👌 IMPROVE:

--- client/src/Book/Pages/Brief/index.js
+++ client/src/Book/Pages/Brief/index.js
@@ -1,5 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
+
+const briefDiv = styled.div`
+  position: absolute;
+  max-width: 1000px;
+  transform: translateX(-50%) translateY(-50%);
+  border-width: 2px 4px 2px 4px; /*top left bottom right */
+  border-style: solid;
+  border-color: beige;
+  border-radius: 5px;
+  background-color: ${(props) => (props.primary ? "azure" : "rgb(40,51,85)")};
+  font-family: "Times New Roman", Times, serif;
+  overflow: visible;
+`;
 
 const mapStatetoProps = (state) => {
   return {
@@ -11,6 +25,12 @@ const mapDispatchToProps = (dispatch) => {
   return {};
 };
 
-const brief = (props) => <p>{props.book.brief}</p>;
+const brief = (props) => (
+  <div>
+    <briefDiv primary={props.theme === "light" ? true : null}>
+      <p>{props.book.brief}</p>
+    </briefDiv>
+  </div>
+);
 
-export default connect(mapStatetoProps,mapDispatchToProps)(brief);
+export default connect(mapStatetoProps, mapDispatchToProps)(brief);
