f6399cd1845aeb777ebfc528ce4a5c7d2ffa4eee
ibrahimshamma99 <i.abushammah@gmail.com>
2020-06-06 23:23:53 +0300

    📦 NEW:

--- client/src/User/Pages/Register/Register.js
+++ client/src/User/Pages/Register/Register.js
@@ -1,12 +1,59 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import "./Signup.css";
+//import Button from "react-bootstrap/Button";
+//import "./Signup.css";
 // import propTypes from "prop-types";
 import { Redirect } from "react-router-dom";
 import RouteNames from "../../constants/user.routes";
 import styled from "styled-components";
+
+//UI
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const RegisterForm = styled.div`
   position: absolute;
@@ -19,7 +66,6 @@ const RegisterForm = styled.div`
   border-radius: 1%;
   background-color: bisque;
 `;
-
 
 class Signup extends React.Component {
   state = {
@@ -169,8 +215,108 @@ class Signup extends React.Component {
   }
 }
 
+const SignUp = (props) => {
+  const SubmitHandler = () => {
+    return props.submit(() => {});
+  };
+  const Changehandler = (name) => (event) => {
+    props.change(name, event.target.value);
+  };
+  const classes = useStyles();
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="first_name"
+            label="First Name"
+            name="first_name"
+            onChange={Changehandler("first_name")}
+            autoComplete="first_name"
+            autoFocus
+          />
 
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="last_name"
+            label="Last Name"
+            name="last_name"
+            onChange={Changehandler("last_name")}
+            autoComplete="last_name"
+            autoFocus
+          />
 
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            onChange={Changehandler("username")}
+            autoComplete="username"
+            autoFocus
+          />
 
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            onChange={Changehandler("email")}
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            onChange={Changehandler("password")}
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            onClick={SubmitHandler}
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+};
 
-export default Signup;
+export default SignUp;
