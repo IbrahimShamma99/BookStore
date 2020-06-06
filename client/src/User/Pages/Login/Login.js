b63d423b72d43f917e75ea08c7010d740105cb90
ibrahimshamma99 <i.abushammah@gmail.com>
2020-06-06 23:09:24 +0300

    👌 IMPROVE:

--- client/src/User/Pages/Login/Login.js
+++ client/src/User/Pages/Login/Login.js
@@ -1,13 +1,136 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+//import Button from "react-bootstrap/Button";
 import "./Login.css";
 import { Redirect } from "react-router-dom";
 import RouteNames from "../../constants/user.routes";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+//UI
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+const SignIn = (props) => {
+  const SubmitHandler = () => {
+    return this.props.submit(() => {});
+  };
+  const Changehandler = (name) => (event) => {
+    props.change(name, event.target.value);
+  };
+  const classes = useStyles();
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            onChange={Changehandler("email")}
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            onChange={Changehandler("password")}
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+};
 const LoginForm = styled.div`
   position: absolute;
   top: 50%;
@@ -18,7 +141,6 @@ const LoginForm = styled.div`
   padding: 20px;
   border-radius: 6px;
   background-color: bisque;
-  
 `;
 
 const Label = styled.label`
@@ -52,76 +174,78 @@ class Login extends React.PureComponent {
   };
 
   render() {
+    const classes = useStyles();
+
     const from = this.props.location.state || RouteNames.base;
     if (this.props.open) {
       return <Redirect to={from} />;
     }
     return (
-      <>
-        <LoginForm>
-          <Form>
-            {this.props.show ? (
-              <div className="alert">
-                <span
-                  className="closebtn"
-                  onClick="this.parentElement.style.display='none';"
-                >
-                  &times;
-                </span>
-                {this.props.error}
-              </div>
-            ) : null}
-            <div
-              className={
-                "form-group" +
-                (this.state.submitted && !this.props.email ? " has-error" : "")
-              }
-            >
-              <Label htmlFor="email">Email</Label>
-              <input
-                type="text"
-                className="form-control"
-                name="email"
-                value={this.props.email}
-                onChange={this.Changehandler("email")}
-                placeholder="Email"
-              />
-
-              {this.state.submitted && !this.props.email && (
-                <div className="help-block">Email is required</div>
-              )}
-            </div>
-            <div
-              className={
-                "form-group" +
-                (this.state.submitted && !this.props.last_name
-                  ? " has-error"
-                  : "")
-              }
-            >
-              <Label htmlFor="password">Password</Label>
-              <input
-                type="password"
-                className="form-control"
-                name="password"
-                value={this.props.password}
-                onChange={this.Changehandler("password")}
-                placeholder="Password"
-              />
-              {this.state.submitted && !this.props.password && (
-                <div className="help-block">Password is required</div>
-              )}
-            </div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              value="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              value="password"
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
             <Button
-              size="md"
-              /*style={}*/ variant="flat"
-              onClick={this.SubmitHandler}
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
             >
-              Submit
-            </Button>{" "}
-          </Form>
-        </LoginForm>
-      </>
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
     );
   }
 }
@@ -130,4 +254,4 @@ Login.propTypes = {
   email: PropTypes.string,
   password: PropTypes.string,
 };
-export default Login;
+export default SignIn;
