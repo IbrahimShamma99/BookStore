858b4deaacab24a6aebb26029ccd8cee01d7b23b
ibrahimshamma99 <i.abushammah@gmail.com>
2020-06-08 13:55:56 +0300

    👌 IMPROVE:

--- client/src/Book/Pages/Create/Create.js
+++ client/src/Book/Pages/Create/Create.js
@@ -131,10 +131,10 @@ const Checkout = (props) => {
                   Thank you for your order.
                 </Typography>
                 <Typography variant="subtitle1">
-                  Your Book number is #2001539 and shipping is no its way 
-                  do you want to confirm?
+                  Your Book number is #2001539 and shipping is no its way do you
+                  want to confirm?
                   <Button
-                    style={{ position: "relative",marginLeft:"30%"}}
+                    style={{ position: "relative", marginLeft: "30%" }}
                     variant="contained"
                     color="primary"
                     className={classes.button}
@@ -142,7 +142,6 @@ const Checkout = (props) => {
                   >
                     Confirm
                   </Button>
-
                 </Typography>
               </React.Fragment>
             ) : (
@@ -172,104 +171,5 @@ const Checkout = (props) => {
     </React.Fragment>
   );
 };
-
-class CreateBook extends React.Component {
-  componentDidMount() {
-    this.props.refreshBook();
-  }
-
-  onChangeHandler = (name) => (event) => {
-    if (name === "cover") {
-      return this.props.change(name, event.target.files[0]);
-    }
-    this.props.change(name, event.target.value);
-  };
-  clickSubmit = (e) => {
-    e.preventDefault();
-    this.props.submit(this.props.userId);
-  };
-
-  render() {
-    return (
-      <div className="update-container">
-        <form>
-          {this.props.open_error ? (
-            <div className="alert">
-              <span className="closebtn">&times;</span>
-              {this.props.error}
-            </div>
-          ) : null}
-
-          {this.props.open_message ? (
-            <div className="alert-success">
-              <span
-                className="closebtn"
-                onClick="this.parentElement.style.display='none';"
-              >
-                &times;
-              </span>
-              {this.props.message}
-            </div>
-          ) : null}
-
-          <label htmlFor="Title">Title:</label>
-          <br />
-          <input
-            value={this.props.title}
-            onChange={this.onChangeHandler("title")}
-            type="text"
-            id="Title"
-            name="Title"
-          ></input>
-          <br />
-
-          <label htmlFor="Brief">Brief:</label>
-          <br />
-          <textarea
-            value={this.props.brief}
-            onChange={this.onChangeHandler("brief")}
-            type="text"
-            id="Brief"
-            name="Brief"
-          ></textarea>
-          <br />
-
-          <label htmlFor="Author">Author:</label>
-          <br />
-          <input
-            value={this.props.author}
-            onChange={this.onChangeHandler("author")}
-            type="text"
-            id="Author"
-            name="Author"
-          ></input>
-          <br />
-          <label htmlFor="genre">Choose genre:</label>
-          <select
-            name="genre"
-            id="genre"
-            onChange={this.onChangeHandler("genre")}
-          >
-            <option value="philosphy">Philosphy</option>
-            <option value="software_development">Software_development</option>
-            <option value="self_improvement">Self_improvement</option>
-          </select>
-
-          <br />
-          <input
-            type="file"
-            className=""
-            name="cover"
-            onChange={this.onChangeHandler("cover")}
-          />
-
-          <Button size="md" variant="flat" onClick={this.clickSubmit}>
-            Submit
-          </Button>
-        </form>
-      </div>
-    );
-  }
-}
 
 export default Checkout;
--- client/src/Book/Pages/Create/from.js
+++ client/src/Book/Pages/Create/from.js
@@ -2,8 +2,6 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import { TextareaAutosize } from "@material-ui/core";
 import { connect } from "react-redux";
 import * as bookTypes from "../../Store/book.actions";
